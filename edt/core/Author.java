a77de9a7d9081a2df656cfd8ee578f214d45af41
Bruno81930 <bruno.castro.machado@tecnico.ulisboa.pt>
2015-11-11 08:06:53 +0000

    Create Class Author, but compareTo method still missing

---
+++ edt/core/Author.java
@@ -0,0 +1,41 @@
+package edt.core;
+
+public class Author{
+	private String _name;
+	private String _email;
+
+	public Author(String name, String email){
+		_name = name;
+		_email = email;
+	}
+
+	public void setName(String name){
+		_name = name;
+	}
+
+	public void setEmail(String email){
+		_email = email;
+	}
+
+	public String getName(){
+		return _name;
+	}
+
+	public String getEmail(){
+		return _email;
+	}
+
+	@Override
+	public boolean equals(Object object){
+		if( object instanceof Author){
+			Author author = (author) object;
+			return _name.equals(author.getName()) && _email.equals(author.getEmail());	
+		}
+		return super.equals(object);
+	}
+
+	@Override
+	public int hashCode(){
+		return (_name + "//" + _email).hashcode();	
+	}
+}
