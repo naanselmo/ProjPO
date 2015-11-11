a0fdef1cd06abe75569393c0cacb229ac45e6d03
Bruno81930 <bruno.castro.machado@tecnico.ulisboa.pt>
2015-11-11 17:54:27 +0000

    Paragraph template

---
+++ edt/core/Paragraph.java
@@ -0,0 +1,14 @@
+package edt.core;
+
+public class Paragraph{
+	private String _text;
+
+	public Paragraph(String text){}
+
+	public String getContent(){}
+
+	public void setText(String text){}
+
+	public String getText(){}
+
+}
