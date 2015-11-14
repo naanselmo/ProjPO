dcfc2973a940cf4658daa380690111a244e72519
Bruno81930 <bruno.castro.machado@tecnico.ulisboa.pt>
2015-11-14 11:58:51 +0000

    Implement ListTopSections

--- src/edt/textui/main/ListTopSections.java
+++ src/edt/textui/main/ListTopSections.java
@@ -1,9 +1,12 @@
 package edt.textui.main;
 
-import edt.core.Editor;
-import pt.utl.ist.po.ui.Command;
 
-/* FIXME: import core classes here */
+import pt.utl.ist.po.ui.Command;
+import pt.utl.ist.po.ui.Display;
+
+import edt.core.Editor;
+import edt.core.Document;
+import edt.core.Section;
 
 /**
  * Command for showing the top sections of the current document in the editor.
@@ -25,6 +28,15 @@ public class ListTopSections extends Command<Editor> {
     @Override
     @SuppressWarnings("nls")
     public final void execute() {
-        /* FIXME: implement command */
+       Document doc = entity().getDocument();
+       Iterator iterator= doc.getSections();
+       Display display = new Display();
+       display.addNewLine(doc.getHeadline());
+       while(iterator.hasNext()){
+            display.addNewLine(iterator.next().getHeadline());
+       }
+
+       display.display();
+
     }
 }
