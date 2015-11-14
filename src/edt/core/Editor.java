772449b4e34b2685fc8d99cd13e7acc1b5e230b3
Bruno81930 <bruno.castro.machado@tecnico.ulisboa.pt>
2015-11-14 11:13:11 +0000

    implemented saveDocument saveNewDocument and loadNewDocument

--- src/edt/core/Editor.java
+++ src/edt/core/Editor.java
@@ -3,7 +3,7 @@ package edt.core;
 /**
  * Created by andre on 11/13/15.
  */
-public class Editor {
+public class Editor implements Serializable{
 
     private Document _document;
 
@@ -12,10 +12,32 @@ public class Editor {
     }
 
     public void saveDocument() {
-
+        saveNewDocument(_document.getPath());
     }
 
-    public void loadDocument() {
+    public void saveNewDocument(String path){
+        _document.setPath(path);
+        try{
+            FileOutputStream file = new FileOutputStream(path);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            out.writeObject(_document);
+            out.close();
+            file.close();
+        }catch(IOException i){
+            i.printStackTrace();
+        }
+    }
+
+    public void loadDocument(String path) {
+        try{
+            FileInputStream file = new FileInputStream(_document.getPath());
+            ObjectInputStream in = new ObjectInputStream(file);
+            _document = in.readObject();
+            in.close();
+            file.close();
+        }catch(IOException i){
+            i.printStackTrace();
+        }
 
     }
 
