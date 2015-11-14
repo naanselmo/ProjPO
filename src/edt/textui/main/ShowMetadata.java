8c1129d6e76baf581c96538b7e738978016119b2
Bruno81930 <bruno.castro.machado@tecnico.ulisboa.pt>
2015-11-14 12:38:00 +0000

    Implemented ShowMetadata

--- src/edt/textui/main/ShowMetadata.java
+++ src/edt/textui/main/ShowMetadata.java
@@ -2,9 +2,14 @@ package edt.textui.main;
 
 import edt.core.Editor;
 import pt.utl.ist.po.ui.Command;
+import pt.utl.ist.po.ui.display;
 
 /* FIXME: import core classes here */
+import edt.core.Document;
+import edt.core.Author;
+import edt.core.Section;
 
+import java.util.Iterator;
 /**
  * Command for showing the metadata of the current document in the editor.
  */
@@ -25,6 +30,21 @@ public class ShowMetadata extends Command<Editor> {
     @Override
     @SuppressWarnings("nls")
     public final void execute() {
-        /* FIXME: implement command */
+        Document document = entity().getDocument();
+        Iterator<Author> authorIterator = document.getAuthors();
+        Display display = new Display();
+
+        display.addNewLine(documentTitle(document.getTitle()));
+
+        while(authorIterator.hasNext()){
+            Author author = authorIterator.next();
+            display.addNewLine(author(author.getTitle(),author.getTitle()));
+        }
+
+        display.addNewLine(documentSections(document.getSectionsCount()));
+        display.addNewLine(documentBytes(document.getDocumentLength()));
+        dispay.addNewLine(documentIdentifiers(document.getTextElementsCount()));
+
+        display.display();
     }
 }
